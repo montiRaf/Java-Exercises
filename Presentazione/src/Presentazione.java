aafec8188f87336102d0a1a850fab9afc16acb84
Raffaele <rafmonti@outlook.it>
2023-10-28 09:58:13 +0200

    Aggiunto primo programma: Presentazione

---
+++ Presentazione/src/Presentazione.java
@@ -0,0 +1,10 @@
+
+public class Presentazione {
+
+	public static void main(String[] args) {
+		System.out.println("Benvenuto, " + args[0] + " " + args[1]);
+		System.out.println("questo e' il tuo primo programma");
+
+	}
+
+}
