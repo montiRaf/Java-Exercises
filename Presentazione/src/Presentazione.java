161bab1289dc3599c300cf39987cf2997d2bd408
Raffaele <rafmonti@outlook.it>
2023-10-28 10:05:42 +0200

    Aggiunta la java doc per Presentazione.java

--- Presentazione/src/Presentazione.java
+++ Presentazione/src/Presentazione.java
@@ -1,10 +1,13 @@
 
 public class Presentazione {
 
+	/**
+	 * 
+	 * @param args array di stringhe contenente le informazioni inserite da linea di comando
+	 */
+	
 	public static void main(String[] args) {
 		System.out.println("Benvenuto, " + args[0] + " " + args[1]);
 		System.out.println("questo e' il tuo primo programma");
-
 	}
-
 }
